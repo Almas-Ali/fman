46623d2e84d3fba390f5428600f9194740e74147
Md. Almas Ali <almaspr3@gmail.com>
2021-12-21 22:31:42 +0600

    Bugfix

--- fman/classx.py
+++ fman/classx.py
@@ -19,6 +19,18 @@ class Version:
     def __str__(self):
         return self.string
 
+    def major(self):
+        '''To get the major version code.'''
+        return int(self.string.split('.')[0])
+
+    def minor(self):
+        '''To get the minor version code.'''
+        return int(self.string.split('.')[1])
+
+    def bugfix(self):
+        '''To get the bugfix version code.'''
+        return int(self.string.split('.')[2])
+
 
 class Author:
     '''To set author string.'''
@@ -40,9 +52,24 @@ class Dev_Status:
         return self.string
 
 
-#a = Author('Almas Ali')
+class Copyright:
+    '''To set copyright string.'''
 
-# print(type(a))
+    def __init__(self, string: str = None):
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+
+class License:
+    '''To set license string.'''
+
+    def __init__(self, string: str = None):
+        self.string = string
+
+    def __str__(self):
+        return self.string
 
 
 class ClassExplore:
@@ -61,6 +88,17 @@ class ClassExplore:
 
     def __dev_status__(self):
         return f'{self.version()} alpha'
+
+    def __copyright__(self):
+        return 'copyright 2021'
+
+    def __license__(self):
+        return '''\
+This is the Licence of this class.
+This class is made by Md. Almas Ali and certified by MIT.
+
+©2021
+'''
 
     def docs(self):
         '''To get docs string.'''
@@ -90,10 +128,16 @@ class ClassExplore:
         except AttributeError:
             raise AttributeError('development status string not found !')
 
+    def copyright(self):
+        '''To get copyright string.'''
+        try:
+            return Copyright(self.cls.__copyright__(self))
+        except AttributeError:
+            raise AttributeError('copyright string not found !')
 
-# c = ClassExplore(ClassExplore)
-
-# #print(c.docs())
-# #print(c.version())
-# #print(c.author())
-# print(type(c.dev_status()))
+    def license(self):
+        '''To get license string.'''
+        try:
+            return License(self.cls.__license__(self))
+        except AttributeError:
+            raise AttributeError('license string not found !')
--- fman/dataset.py
+++ fman/dataset.py
@@ -1,82 +1,125 @@
-class Dataset:
-    '''Dataset is a custom datatype for global use case.'''
+class Array:
+    '''Array is a custom datatype for global use case.'''
 
-    def __init__(self, list: list = None):
-        '''Dataset takes only a list of different values.'''
-        if list == None:
+    def __init__(self, *args):
+        '''Array takes different values to create arrays.'''
+        if args is None:
             self.list = []
-        self.list = list
+        else:
+            self.list = list(args)
 
     def push(self, value: any):
-        '''To add a new value in dataset to end.'''
-        return self.list.append(value)
+        '''To add a new value in array to end.'''
+        self.list.append(value)
 
-    def insert(self, position: int = 0, value: any = ''):
-        '''To insert a value in dataset in anywhere. As default it add in front.'''
-        return self.list.insert(position, value)
+    def insert(self, position: int = 0, value: any = None):
+        '''To insert a value in array in anywhere. As default it add in front.'''
+        self.list.insert(position, value)
 
     def pop(self):
-        '''To remove last value of dataset.'''
-        return self.list.pop()
+        '''To remove last value of array.'''
+        self.list = self.list[:-1:]
+        # return self.list
 
     def peek(self, index: int):
         '''To get a value by its index number.'''
         return self.list[index]
 
     def remove(self, value: any):
-        '''To remove a value from dataset.'''
-        return self.list.remove(value)
+        '''To remove a value from array.'''
+        self.list.remove(value)
 
     def find(self, value: any):
-        '''To find any value in dataset.'''
+        '''To find any value in array.'''
         num = 0
         for l in self.list:
             if l == value:
                 return num
             num += 1
+        return -1
 
     def count(self, value: any):
-        '''To count any repeated value in dataset.'''
-        return self.list.count(value)
+        '''To count any repeated value in array.'''
+        # return self.list.count(value)
+        num = 0
+        for l in self.list:
+            if l == value:
+                num += 1
+        return num
 
     def sort(self):
-        '''To sort the dataset.'''
-        return self.list.sort()
+        '''To sort the array.'''
+        self.__cast(self.list).sort()
 
     def reverse(self):
-        '''To reverse dataset.'''
-        return self.list.reverse()
+        '''To reverse array.'''
+        self.list = self.list[::-1]
 
     def clear(self):
-        '''To clear dataset.'''
-        return self.list.clear()
+        '''To clear array.'''
+        self.list = []
 
     def len(self):
-        '''To get length of dataset.'''
+        '''To get length of array.'''
         return len(self.list)
 
     def slice(self, start: int = None, end: int = None, jump: int = None):
-        '''To slice dataset.'''
+        '''To slice array.'''
         return self.__return_value(self.list[start:end:jump])
 
+    def q(self):
+        '''To set a sequential task.'''
+    pass
+
+    def foreach(self, callback):
+        '''Looping all values.'''
+        num = 0
+        for val in self.list:
+            num += 1
+            try:
+                callback(val)
+            except:
+                try:
+                    callback(val, num)
+                except:
+                    pass
+
+    def typecast(self, value: any):
+        '''To type cast anything to Array object.'''
+        if type(value) == list or type(value) == set or type(value) == tuple:
+            a = Array()
+            for v in value:
+                a.push(v)
+            return a
+        elif type(value) == dict:
+            raise ValueError('can\'t typecast dict object.')
+        else:
+            raise TypeError('unknown value receive.')
+
     def __str__(self):
-        '''To return dataset object.'''
+        '''To return array object.'''
         return f'<{str(self.list)[1:-1]}>'
 
-    def __add__(self, value):
-        '''To add dataset instances.'''
-        return Dataset(self.list + str(value)[1:-1].replace('\'', '').split(','))
+    def __add__(self, *value):
+        '''To add array instances.'''
+        arr = Array()
+        for x in self.list:
+            arr.push(x)
+        for x in value:
+            for b in x:
+                arr.push(b)
+        return arr
 
     def __getitem__(self, position):
-        '''To get dataset iterate able and sliceable.'''
+        '''To get array iterate able and sliceable.'''
         return self.list[position]
 
     def __len__(self):
-        '''To get len of dataset.'''
+        '''To get len of array.'''
         return len(self.list)
 
     def __reversed__(self):
-        '''To reversed the dataset instance.'''
+        '''To reversed the array instance.'''
         return self.list[::-1]
 
     def __repr__(self):
@@ -84,59 +127,10 @@ class Dataset:
         return f'<{str(self.list)[1:-1]}>'
 
     def __cast(self, value):
-        '''To cast dataset object to list. (Internal use only)'''
-        return list(value[1:-1])
+        '''To cast array object to list. (Internal use only)'''
+        return str(value)[1:-1].replace('\'', '').split(',')
 
     @staticmethod
     def __return_value(value):
-        '''To get dataset instance. (Internal use only)'''
+        '''To get array instance. (Internal use only)'''
         return f'<{str(value)[1:-1]}>'
-
-    @staticmethod
-    def __get_value(value):
-        '''To get dataset value. (Internal use only)'''
-        return f'{str(value)[1:-1]}'
-
-
-v = Dataset(['Almas', 'Ali', '8262'])
-
-v.push('pranto')
-
-v.pop()
-
-v.push('tom')
-
-c = v.count('tom')
-
-f = v.find('Ali')
-
-p = v.peek(3)
-
-v.sort()
-
-v.reverse()
-
-# v.clear()
-
-#v = v.slice(1,2,4)
-
-#v = v.__return_value()
-
-#v = v+v+v+v
-
-#v = v.len()
-
-
-#v= Dataset()
-
-# print(type(v))
-# print(len(v))
-
-# for b in v:
-#	print(b)
-
-# print(v[2])
-
-# print(reversed(v))
-
-print(v)
